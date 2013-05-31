a829eb2e077037b60d2dc1c909c63dc2365555e4
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 07:48:09 +0200

    [tests] test autofs::config with custom parameters

--- spec/classes/autofs_config_spec.rb
+++ spec/classes/autofs_config_spec.rb
@@ -3,5 +3,13 @@ require 'spec_helper'
 describe 'autofs::config', :type => :class  do
   let (:facts) { { :osfamily => 'RedHat' } }
 
-  it { should contain_file('/etc/auto.master').with_mode('0644').with_owner('root').with_group('root') }
+  describe 'with default parameters' do
+    it { should contain_file('/etc/auto.master').with_mode('0644').with_owner('root').with_group('root') }
+  end
+
+  describe 'with custom parameters' do
+    let (:params) { { :mode   => '0640', } }
+
+    it { should contain_file('/etc/auto.master').with_mode('0640') }
+  end
 end
