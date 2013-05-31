e6ca81ccb69c562af971ddd1d95abb7a3fa0da86
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 12:34:36 +0200

    [ruby] use rspec-puppet-augeas with fix for lensdir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.augeas_fixtures = File.join(fixture_path, 'augeas')
+  c.augeas_lensdir = File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib/augeas/lenses')
 end
