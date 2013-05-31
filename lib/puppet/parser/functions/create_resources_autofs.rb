37e6d69c9a657bf24de1abe8e7b4e025df356142
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 12:13:39 +0200

    [module] parse the tree we get from hiera for autofs::loader

--- lib/puppet/parser/functions/create_resources_autofs.rb
+++ lib/puppet/parser/functions/create_resources_autofs.rb
@@ -16,6 +16,20 @@ module Puppet::Parser::Functions
     autofs_includes = {}
     autofs_mounts = {}
 
+    # this will remove the shares parameter from the hash
+    # and create autofs::mounts from the definitions there
+    tree.each do |incl, params|
+      if params.is_a?(Hash)
+        shares = params.delete('shares') || {}
+        autofs_includes[incl] = params
+        shares.each do |m,p|
+          # override the map to the parent autofs::include
+          p['map'] = incl
+          autofs_mounts[m] = p
+        end
+      end
+    end
+
     function_create_resources(['autofs::include', autofs_includes])
     function_create_resources(['autofs::mount', autofs_mounts])
   end
