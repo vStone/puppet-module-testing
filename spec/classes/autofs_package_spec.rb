01d2fea9f5c114aec6a87492111fc3d6683d6292
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 07:46:21 +0200

    [tests] test autofs::package inheritance

--- spec/classes/autofs_package_spec.rb
+++ spec/classes/autofs_package_spec.rb
@@ -4,5 +4,12 @@ require 'spec_helper'
 describe 'autofs::package' do
   let (:facts) { { :osfamily => 'RedHat' } }
 
-  it { should contain_package('autofs').with_alias('autofs').with_ensure('installed') }
+  describe 'default' do
+    it { should contain_package('autofs').with_alias('autofs').with_ensure('installed') }
+  end
+
+  describe 'inherit from autofs' do
+    let (:pre_condition) { "class {'autofs': package => 'foobar', }" }
+    it { should contain_package('foobar').with_alias('autofs').with_ensure('installed') }
+  end
 end
