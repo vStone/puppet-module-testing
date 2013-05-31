762a5c5e6762f6db5474492bb9b46f2ff74ff085
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 07:52:19 +0200

    [tests] test autofs::service inheritance

--- spec/classes/autofs_service_spec.rb
+++ spec/classes/autofs_service_spec.rb
@@ -3,5 +3,12 @@ require 'spec_helper'
 describe 'autofs::service', :type => :class do
   let (:facts) { { :osfamily => 'RedHat' } }
 
-  it { should contain_service('autofs').with_alias('autofs') }
+  describe 'default' do
+    it { should contain_service('autofs').with_alias('autofs') }
+  end
+
+  describe 'inherit from autofs' do
+    let (:pre_condition) { "class {'autofs': service => 'foobar', }" }
+    it { should contain_service('foobar').with_alias('autofs') }
+  end
 end
