554b39bcd5574f089519ea32279d103ecebb8c99
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 12:11:55 +0200

    Module autofs now has a autofs::loader which will load stuff from hiera.

--- puppet/manifests/nodes/test.pp
+++ puppet/manifests/nodes/test.pp
@@ -1,11 +1,5 @@
 node /^test/ {
 
-  include autofs
-  autofs::include {'test': }
-  autofs::mount {'foo':
-    map      => 'test',
-    host     => 'localhost',
-    location => '/bar',
-  }
+  include autofs::loader
 
 }
