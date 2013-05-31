4c8e1c33b3ea179595e601a07846867ff2ed2dee
Jan Vansteenkiste <jan@vstone.eu>
2013-05-31 09:27:51 +0200

    [module] use augeas to include a mapfile
    * Added the automaster augeas lens from:
      https://github.com/hercules-team/augeas/blob/master/lenses/automaster.aug
    * Use augeas to add/update/remove.

--- manifests/include.pp
+++ manifests/include.pp
@@ -64,15 +64,43 @@ define autofs::include (
     before => Service['autofs'],
   }
 
+  # For all augeas stuff we use.
+  Augeas {
+    incl    => $_masterfile,
+    lens    => 'Automaster.lns',
+  }
+
+  $context = "*[label() != '#comment'][. = '${_mount}']"
+
   case $ensure {
+    'present',default: {
+
+      augeas {"autofs::include::add-${_mount}":
+        changes => [
+          "set 01 '${_mount}'",
+          "set 01/map '${_mapfile}'",
+        ],
+        onlyif  => "match ${context} size == 0",
+      }
+
+      augeas{"autofs::include::update-${_mount}":
+        before  => Augeas["autofs::include::add-${_mount}"],
+        changes => "set ${context}/map '${_mapfile}'",
+        onlyif  => "match ${context} size > 0",
+      }
+
+      File[$_mapfile] { ensure => 'present', }
+
+    }
     'absent': {
+      # remove the entry.
+      augeas {"autofs::include::rm-${_mount}":
+        changes => "rm ${context}",
+      }
       # if purge is enabled, remove the mapfile too.
       if $purge {
         File[$_mapfile] { ensure => 'absent', }
       }
-    }
-    'present',default: {
-      File[$_mapfile] { ensure => 'present', }
     }
 
   }
